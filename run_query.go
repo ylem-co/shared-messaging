e4bba51f620cdc71c953296430aba1a89990a141
Oliver Schäfer <oliver@olschafer.com>
2021-12-14 12:54:17 +0100

    updated source declaration

--- run_query.go
+++ run_query.go
@@ -14,5 +14,33 @@ type RunQueryTask struct {
 }
 
 type Source struct {
-	Uuid uuid.UUID `json:"uuid"`
+	Uuid             uuid.UUID `json:"uuid"`
+	CreatorUuid      uuid.UUID `json:"creator_uuid"`
+	OrganizationUuid uuid.UUID `json:"organization_uuid"`
+	Status           string    `json:"status"`
+	Type             string    `json:"type"`
+	Name             string    `json:"name"`
+	Host             string    `json:"host"`
+	Port             int       `json:"port,omitempty"`
+	User             string    `json:"user,omitempty"`
+	Password         string    `json:"password"`
+	Database         string    `json:"database,omitempty"`
+	ConnectionType   string    `json:"connection_type"`
+	SshHost          string    `json:"ssh_host,omitempty"`
+	SshPort          int       `json:"ssh_port,omitempty"`
+	SshUser          string    `json:"ssh_user,omitempty"`
 }
+
+const SourceStatusNew = "new"
+const SourceStatusOnline = "online"
+const SourceStatusOffline = "offline"
+
+const SourceTypeMySQL = "mysql"
+const SourceTypeSnowflake = "snowflake"
+const SourceTypePostgresql = "postgresql"
+const SourceTypeAWSRDS = "aws-rds"
+const SourceTypeGoogleCloudSQL = "google-cloud-sql"
+const SourceTypeMicrosoftAzureSQL = "microsoft-azure-sql"
+
+const SourceConnectionTypeDirect = "direct"
+const SourceConnectionTypeSsh = "ssh"
