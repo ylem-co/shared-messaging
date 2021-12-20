c783cda4115d912b3270e8ac5c497ebe0fb5ec87
Oliver Schäfer <oliver@olschafer.com>
2021-12-20 17:06:36 +0100

    api destination

--- call_api.go
+++ call_api.go
@@ -4,10 +4,20 @@ const TaskCallApiMessageName = "tasks.call_api"
 
 type CallApiTask struct {
 	Task
-	Type             string      `json:"type"`
-	Payload          string      `json:"payload"`
-	QueryString      string      `json:"query_string"`
-	Headers          string      `json:"headers"`
-	AttachedFileName string      `json:"attached_file_name"`
-	Destination      Destination `json:"destination"`
+	Type             string         `json:"type"`
+	Payload          string         `json:"payload"`
+	QueryString      string         `json:"query_string"`
+	Headers          string         `json:"headers"`
+	AttachedFileName string         `json:"attached_file_name"`
+	Destination      ApiDestination `json:"destination"`
+}
+
+type ApiDestination struct {
+	Destination
+	AuthType              string `json:"auth_type"`
+	AuthBearerToken       string `json:"auth_bearer_token"`
+	AuthBasicUserName     string `json:"auth_basic_user_name"`
+	AuthBasicUserPassword string `json:"auth_basic_user_password"`
+	AuthHeaderName        string `json:"auth_header_name"`
+	AuthHeaderValue       string `json:"auth_header_value"`
 }
