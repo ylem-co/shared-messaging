d1e02d1635c0ace8eb9a1d1aac053e7d2e491a6d
schneekatze <schneekatze@users.noreply.github.com>
2022-05-19 09:52:07 +0200

    add org uuid (#23)

--- customers/registered.go
+++ customers/registered.go
@@ -9,6 +9,7 @@ type CustomerRegistered struct {
 	Email            string `json:"email"`
 	Phone            *string `json:"phone"`
 	OrganizationName string `json:"organization_name"`
+	OrganizationUuid string `json:"organization_uuid"`
 }
 
 func (m *CustomerRegistered) GetMacawMessageKey() string {
